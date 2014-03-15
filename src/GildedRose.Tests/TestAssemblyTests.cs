c758fcf6cb7afc454788d11cf816ad24bd2eadae
vigarcia <vigarcia@plainconcepts.com>
2014-03-15 22:39:19 +0100

    OnceTheSellByDateHasPassedQualityDegradesTwiceAsFast -> green

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -30,6 +30,17 @@ namespace GildedRose.Tests
             item.Quality.Should().Be(19);
         }
 
+        [Test]
+        public void OnceTheSellByDateHasPassedQualityDegradesTwiceAsFast()
+        {
+            var item = CreateItem(sellIn: 0, quality: 20);
+            var program = new Program();
+
+            program.UpdateQuality(new List<Item> { item });
+
+            item.Quality.Should().Be(18);
+        }
+
         private Item CreateItem(string name = "Regular item", int quality = 20, int sellIn = 30)
         {
             return new Item { Name = name, Quality = quality, SellIn = sellIn };
