3a99e200c4e5f4dac5de9be4fca186746202f5fe
vigarcia <vigarcia@plainconcepts.com>
2014-03-15 22:35:55 +0100

    Refactor

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -11,23 +11,28 @@ namespace GildedRose.Tests
         [Test]
         public void AtTheEndOfEachDayTheSystemLowersSellInByOne()
         {
-            var item = new Item { Name = "Regular item", Quality = 20, SellIn = 20 };
+            var item = CreateItem(sellIn: 20);
             var program = new Program();
 
             program.UpdateQuality(new List<Item> { item });
 
-            item.SellIn.Should().Be(19);            
+            item.SellIn.Should().Be(19);
         }
 
         [Test]
         public void AtTheEndOfEachDayTheSystemLowersQualityInByOne()
         {
-            var item = new Item { Name = "Regular item", Quality = 20, SellIn = 20 };
+            var item = CreateItem(quality: 20);
             var program = new Program();
 
             program.UpdateQuality(new List<Item> { item });
 
             item.Quality.Should().Be(19);
+        }
+
+        private Item CreateItem(string name = "Regular item", int quality = 20, int sellIn = 30)
+        {
+            return new Item { Name = name, Quality = quality, SellIn = sellIn };
         }
     }
 }
