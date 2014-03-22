65ab1b7041973ca8567e3d8cc56cffea554a33ef
vigarcia <vigarcia@plainconcepts.com>
2014-03-22 18:38:49 +0100

    Extract item to its own class

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -68,14 +68,4 @@ namespace GildedRose.Console
             }
         }
     }
-
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
-
 }
